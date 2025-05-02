52488e9f09ae0887684debcb8f560135274f7213
Alexs Feng <168613023+Alex21FW@users.noreply.github.com>
2025-05-01 20:35:18 -0600

    nuevo mapa, nuevos enemigo, y inicio oleada

--- demo/gdextensiondemo/Enemigos/Ogro/ogro.gd
+++ demo/gdextensiondemo/Enemigos/Ogro/ogro.gd
@@ -2,5 +2,5 @@ extends "res://Enemigos/CualidadesBaseEnemigo.gd"
 
 func _ready():
 	health = 200
-	speedOfMovement = 80.0
+	speedOfMovement = 300.0
 	super._ready() # Llama al _ready() de la clase base
--- demo/gdextensiondemo/main.gd
+++ demo/gdextensiondemo/main.gd
@@ -4,12 +4,22 @@ extends Node2D
 @onready var ground_layer = $TileMap/GroundLayer
 @onready var obstacle_layer = $TileMap/ObstacleLayer
 @onready var pathfinder = $Pathfinder
-@onready var enemy_scene = preload("res://Enemigos/Ogro/Ogro.tscn")
+
 @onready var spawn_timer = $SpawnTimer
+@onready var wave_timer = $WaveDelayTimer
+
+@onready var ogro_scene = preload("res://Enemigos/Ogro/Ogro.tscn")
+@onready var elfo_scene = preload("res://Enemigos/Elfo/Elfo.tscn")
+
+var current_wave = 0
+var enemies_to_spawn = []
+var enemies_alive = 0
 
 func _ready():
 
-	spawn_timer.timeout.connect(_on_spawn_timer_timeout)
+	spawn_timer.timeout.connect(_on_SpawnTimer_timeout)
+	wave_timer.timeout.connect(_on_WaveDelayTimer_timeout)
+	
 
 	print("Inicializando Pathfinder...")
 
@@ -30,7 +40,62 @@ func _ready():
 			if tile_id != -1:
 				pathfinder.set_obstacle(tile_pos, false)  # false = no caminable
 				print("Marcado como obstáculo:", tile_pos)
+				
+	start_next_wave()
 
-func _on_spawn_timer_timeout():
-	var enemy = enemy_scene.instantiate()
-	add_child(enemy)
+func start_next_wave():
+	current_wave += 1
+	print("Inciando oleada %d" % current_wave)
+	
+	match current_wave:
+		1:
+			var ogros = []
+			for i in range(5 * current_wave):
+				ogros.append("ogro")
+			enemies_to_spawn = ogros
+		2:
+			var ogros = []
+			for i in range(10 * current_wave):
+				ogros.append("ogro")
+			var elfos = []
+			for i in range(5 * current_wave):
+				elfos.append("elfo")
+			enemies_to_spawn = ogros + elfos
+		_:
+			var ogros = []
+			for i in range(5 * current_wave):
+				ogros.append("ogro")
+			var elfos = []
+			for i in range(2 * current_wave):
+				elfos.append("elfo")
+			enemies_to_spawn = ogros + elfos
+	
+	enemies_alive = enemies_to_spawn.size()
+	spawn_timer.start()
+	
+func _on_SpawnTimer_timeout():
+	if enemies_to_spawn.size() == 0:
+		spawn_timer.stop()
+		return
+		
+	var type = enemies_to_spawn.pop_front()
+	var enemy
+	
+	match type:
+		"ogro":
+			enemy = ogro_scene.instantiate()
+		"elfo":
+			enemy = elfo_scene.instantiate()
+			
+	if enemy:
+		enemy.position = ground_layer.map_to_local(Vector2i(0,0))
+		add_child(enemy)
+		
+func _on_enemy_died():
+	enemies_alive -= 1
+	if enemies_alive <= 0:
+		print("Todos los enemigos derrotados.")
+		wave_timer.start(10)
+			
+func _on_WaveDelayTimer_timeout():
+	start_next_wave()
